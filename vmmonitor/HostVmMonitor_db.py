21c3f8ea4c9871fa0a38dbd172ebfb0627bd9a30
kevinjs <dysj4099@gmail.com>
2013-04-02 17:39:37 +0800

    none_key_error

--- vmmonitor/HostVmMonitor_db.py
+++ vmmonitor/HostVmMonitor_db.py
@@ -133,98 +133,100 @@ class HostVmMonitor(Daemon):
     '''
     def check_vm(self, chk_timestamp):
         tmp = self._rc.run("nova --os-username admin --os_password csdb123cnic --os_tenant_name admin --os_auth_url http://192.168.138.32:5000/v2.0 list ")
-        tmps = tmp.strip().split('\n')
+
+	if tmp != None and tmp != '' and tmp != '\n':
+            tmps = tmp.strip().split('\n')
+            id_list = {}
+            redis_c = redis.Redis(host='127.0.0.1', port=6379)
         
-        id_list = {}
-        redis_c = redis.Redis(host='127.0.0.1', port=6379)
-        
-        for line in tmps:
-            if not line.startswith('+'):
-                line_s = line.strip().split('|')
-                if line_s[1].strip() != 'ID':
-                    tmp_dict = {}
-                    tmp_dict['ins_uuid'] = line_s[1].strip()
-                    tmp_dict['ins_name'] = line_s[2].strip()
-                    tmp_dict['ins_status'] = line_s[3].strip()
+            for line in tmps:
+                if not line.startswith('+'):
+                    line_s = line.strip().split('|')
+                    if line_s[1].strip() != 'ID':
+                        tmp_dict = {}
+                        tmp_dict['ins_uuid'] = line_s[1].strip()
+                        tmp_dict['ins_name'] = line_s[2].strip()
+                        tmp_dict['ins_status'] = line_s[3].strip()
 #                    tmp_dict['ins_hostname'] = self._rc.run("nova --os-username admin --os_password csdb123cnic --os_tenant_name admin --os_auth_url http://192.168.138.32:5000/v2.0 show " + tmp_dict['ins_uuid'] + " | grep OS-EXT-SRV-ATTR:hypervisor_hostname | awk '{print $4}'").strip()
-                    tmp_dict['ins_hostname'] = self._find_host(tmp_dict['ins_uuid'])
-                    tmp_dict['chk_time'] = chk_timestamp #check timestamp
+                        tmp_dict['ins_hostname'] = self._find_host(tmp_dict['ins_uuid'])
+                        tmp_dict['chk_time'] = chk_timestamp #check timestamp
                     
-                    # select from redis db
-                    ins_stat_str = redis_c.lindex(line_s[1].strip(), -1)
+                        # select from redis db
+                        ins_stat_str = redis_c.lindex(line_s[1].strip(), -1)
                     
-                    if ins_stat_str != None and ins_stat_str != '':
-                        # "0.590$938392$2097152$vnet2:14844296$vnet2:106438$hda:681078784$hda:531002368$1364366831"
-                        ins_stat = ins_stat_str.split('$')
+                        if ins_stat_str != None and ins_stat_str != '':
+                            # "0.590$938392$2097152$vnet2:14844296$vnet2:106438$hda:681078784$hda:531002368$1364366831"
+                            ins_stat = ins_stat_str.split('$')
 
-                        tmp_dict['cpu_usage'] = ins_stat[0]
-                        tmp_dict['mem_free'] = ins_stat[1]
-                        tmp_dict['mem_max'] = ins_stat[2]
-                        tmp_dict['nic_in'] = ins_stat[3]
-                        tmp_dict['nic_out'] = ins_stat[4]
-                        tmp_dict['disk_read'] = ins_stat[5]
-                        tmp_dict['disk_write'] = ins_stat[6]
+                            tmp_dict['cpu_usage'] = ins_stat[0]
+                            tmp_dict['mem_free'] = ins_stat[1]
+                            tmp_dict['mem_max'] = ins_stat[2]
+                            tmp_dict['nic_in'] = ins_stat[3]
+                            tmp_dict['nic_out'] = ins_stat[4]
+                            tmp_dict['disk_read'] = ins_stat[5]
+                            tmp_dict['disk_write'] = ins_stat[6]
 
-                        tmp_dict['monitor_time'] = ins_stat[len(ins_stat) - 1]
-                    else:
-                        tmp_dict['monitor_time'] = -1 # Error: no record in redis
-                    tmp_dict['repeat_num'] = 0
+                            tmp_dict['monitor_time'] = ins_stat[len(ins_stat) - 1]
+                        else:
+                            tmp_dict['monitor_time'] = -1 # Error: no record in redis
+                        tmp_dict['repeat_num'] = 0
                     
-                    id_list[line_s[1].strip()] = tmp_dict
+                        id_list[line_s[1].strip()] = tmp_dict
         
-        # Update global vm_list
-        if id_list != None and len(id_list) != 0:
-            for vm_c in id_list: # Notice: vm_c is uuid
-                if self._vm_list.has_key(vm_c):
-                    # Update
-                    self._vm_list[vm_c]['ins_status'] = id_list[vm_c]['ins_status']
-                    self._vm_list[vm_c]['ins_hostname'] = id_list[vm_c]['ins_hostname']
-                    self._vm_list[vm_c]['chk_time'] = id_list[vm_c]['chk_time'] #Check time
+            # Update global vm_list
+            if id_list != None and len(id_list) != 0:
+                for vm_c in id_list: # Notice: vm_c is uuid
+                    if self._vm_list.has_key(vm_c):
+                        # Update
+                        self._vm_list[vm_c]['ins_status'] = id_list[vm_c]['ins_status']
+                        self._vm_list[vm_c]['ins_hostname'] = id_list[vm_c]['ins_hostname']
+                        self._vm_list[vm_c]['chk_time'] = id_list[vm_c]['chk_time'] #Check time
 		    
-		    if id_list[vm_c].has_key('cpu_usage'):                    
-                        self._vm_list[vm_c]['cpu_usage'] = id_list[vm_c]['cpu_usage']
-                        self._vm_list[vm_c]['mem_free'] = id_list[vm_c]['mem_free']
-                        self._vm_list[vm_c]['mem_max'] = id_list[vm_c]['mem_max']
-                        self._vm_list[vm_c]['nic_in'] = id_list[vm_c]['nic_in']
-                        self._vm_list[vm_c]['nic_out'] = id_list[vm_c]['nic_out']
-                        self._vm_list[vm_c]['disk_read'] = id_list[vm_c]['disk_read']
-                        self._vm_list[vm_c]['disk_write'] = id_list[vm_c]['disk_write']
+		        if id_list[vm_c].has_key('cpu_usage'):                    
+                            self._vm_list[vm_c]['cpu_usage'] = id_list[vm_c]['cpu_usage']
+                            self._vm_list[vm_c]['mem_free'] = id_list[vm_c]['mem_free']
+                            self._vm_list[vm_c]['mem_max'] = id_list[vm_c]['mem_max']
+                            self._vm_list[vm_c]['nic_in'] = id_list[vm_c]['nic_in']
+                            self._vm_list[vm_c]['nic_out'] = id_list[vm_c]['nic_out']
+                            self._vm_list[vm_c]['disk_read'] = id_list[vm_c]['disk_read']
+                            self._vm_list[vm_c]['disk_write'] = id_list[vm_c]['disk_write']
                     
-                    # Last report time by kanyun
-                    if self._vm_list[vm_c]['monitor_time'] == id_list[vm_c]['monitor_time']:
-                        # add repeat num
-                        self._vm_list[vm_c]['repeat_num'] = self._vm_list[vm_c]['repeat_num'] + 1
-                        self._report[self._vm_list[vm_c]['ins_hostname']]['vm_list'].append({'vm':vm_c, 'status':'WAITING', 'repeat_num': self._vm_list[vm_c]['repeat_num'], 'status-ori':self._vm_list[vm_c]['ins_status']})
+                        # Last report time by kanyun
+                        if self._vm_list[vm_c]['monitor_time'] == id_list[vm_c]['monitor_time']:
+                            # add repeat num
+                            self._vm_list[vm_c]['repeat_num'] = self._vm_list[vm_c]['repeat_num'] + 1
+                            self._report[self._vm_list[vm_c]['ins_hostname']]['vm_list'].append({'vm':vm_c, 'status':'WAITING', 'repeat_num': self._vm_list[vm_c]['repeat_num'], 'status-ori':self._vm_list[vm_c]['ins_status']})
+                        else:
+                            # set repeat num to zero
+                            self._vm_list[vm_c]['repeat_num'] = 0
+                            self._vm_list[vm_c]['monitor_time'] = id_list[vm_c]['monitor_time']
+                            self._report[self._vm_list[vm_c]['ins_hostname']]['vm_list'].append({'vm':vm_c, 'status':'OK', 'repeat_num': self._vm_list[vm_c]['repeat_num'], 'status-ori':self._vm_list[vm_c]['ins_status']})   
                     else:
-                        # set repeat num to zero
-                        self._vm_list[vm_c]['repeat_num'] = 0
-                        self._vm_list[vm_c]['monitor_time'] = id_list[vm_c]['monitor_time']
-                        self._report[self._vm_list[vm_c]['ins_hostname']]['vm_list'].append({'vm':vm_c, 'status':'OK', 'repeat_num': self._vm_list[vm_c]['repeat_num'], 'status-ori':self._vm_list[vm_c]['ins_status']})   
-                else:
-                    # Insert
-                    tmp_dict = {}
-                    tmp_dict['ins_uuid'] = id_list[vm_c]['ins_uuid']
-                    tmp_dict['ins_name'] = id_list[vm_c]['ins_name']
-                    tmp_dict['ins_status'] = id_list[vm_c]['ins_status']
+                        # Insert
+                        tmp_dict = {}
+                        tmp_dict['ins_uuid'] = id_list[vm_c]['ins_uuid']
+                        tmp_dict['ins_name'] = id_list[vm_c]['ins_name']
+                        tmp_dict['ins_status'] = id_list[vm_c]['ins_status']
 #                    tmp_dict['ins_hostname'] = self._rc.run("nova --os-username admin --os_password csdb123cnic --os_tenant_name admin --os_auth_url http://192.168.138.32:5000/v2.0 show " + tmp_dict['ins_uuid'] + " | grep OS-EXT-SRV-ATTR:hypervisor_hostname | awk '{print $4}'").strip()
-                    tmp_dict['ins_hostname'] = self._find_host(tmp_dict['ins_uuid'])
+                        tmp_dict['ins_hostname'] = self._find_host(tmp_dict['ins_uuid'])
 
-#		     print id_list[vm_c]
-		    if id_list[vm_c].has_key('cpu_usage'):
-                        tmp_dict['cpu_usage'] = id_list[vm_c]['cpu_usage']
-                        tmp_dict['mem_free'] = id_list[vm_c]['mem_free']
-                        tmp_dict['mem_max'] = id_list[vm_c]['mem_max']
-                        tmp_dict['nic_in'] = id_list[vm_c]['nic_in']
-                        tmp_dict['nic_out'] = id_list[vm_c]['nic_out']
-                        tmp_dict['disk_read'] = id_list[vm_c]['disk_read']
-                        tmp_dict['disk_write'] = id_list[vm_c]['disk_write']
+#		        print id_list[vm_c]
+		        if id_list[vm_c].has_key('cpu_usage'):
+                            tmp_dict['cpu_usage'] = id_list[vm_c]['cpu_usage']
+                            tmp_dict['mem_free'] = id_list[vm_c]['mem_free']
+                            tmp_dict['mem_max'] = id_list[vm_c]['mem_max']
+                            tmp_dict['nic_in'] = id_list[vm_c]['nic_in']
+                            tmp_dict['nic_out'] = id_list[vm_c]['nic_out']
+                            tmp_dict['disk_read'] = id_list[vm_c]['disk_read']
+                            tmp_dict['disk_write'] = id_list[vm_c]['disk_write']
 
-                    tmp_dict['chk_time'] = id_list[vm_c]['chk_time']
-                    tmp_dict['monitor_time'] = id_list[vm_c]['monitor_time']
-                    tmp_dict['repeat_num'] = 0
+                        tmp_dict['chk_time'] = id_list[vm_c]['chk_time']
+                        tmp_dict['monitor_time'] = id_list[vm_c]['monitor_time']
+                        tmp_dict['repeat_num'] = 0
                     
-                    self._vm_list[vm_c] = tmp_dict
-                    self._report[self._vm_list[vm_c]['ins_hostname']]['vm_list'].append({'vm':vm_c, 'status':'NEW', 'repeat_num': self._vm_list[vm_c]['repeat_num'], 'status-ori':self._vm_list[vm_c]['ins_status']})
+                        self._vm_list[vm_c] = tmp_dict
+                        self._report[self._vm_list[vm_c]['ins_hostname']]['vm_list'].append({'vm':vm_c, 'status':'NEW', 'repeat_num': self._vm_list[vm_c]['repeat_num'], 'status-ori':self._vm_list[vm_c]['ins_status']})
+
     '''
     Chech around
     '''
